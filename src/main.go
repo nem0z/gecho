b09645b6d4cc465e4d404c3f6fa66ca021d36475
nem0z <maxime.bernard.dev@gmail.com>
2024-03-27 01:19:10 +0100

    feat (src/main): update main

--- src/main.go
+++ src/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/nem0z/gecho/message"
 	"github.com/nem0z/gecho/message/payloads"
 	"github.com/nem0z/gecho/peer"
+	"github.com/nem0z/gecho/peer/handlers"
 	"github.com/nem0z/gecho/server"
 )
 
@@ -24,13 +25,19 @@ func main() {
 	log.Println("Server started :", serv)
 
 	p, err := peer.New("localhost:8080")
+	p.Register("echo", handlers.Echo(p))
+	p.Register("ping", handlers.Ping(p))
+	p.Register("pong", handlers.Pong(p))
 	handle(err)
 
 	time.Sleep(time.Second)
 
-	echo := message.Format("echo", []byte("Hello, world!"))
-	fmt.Println("Message :", echo)
-	err = p.Send(echo)
+	echo := payloads.NewEcho([]byte("Hello, World!"))
+	msg, err := message.Format("echo", echo)
+	handle(err)
+
+	fmt.Println("Message :", msg)
+	err = p.Send(msg)
 	handle(err)
 
 	select {}
