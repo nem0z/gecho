83d5aa2d8c2e2f4e97a47bfaa6dca05790d34680
nem0z <maxime.bernard.dev@gmail.com>
2024-03-28 11:19:22 +0100

    refactor (src/peer): split peer.go

--- src/peer/handler.go
+++ src/peer/handler.go
@@ -3,3 +3,7 @@ package peer
 import "github.com/nem0z/gecho/message"
 
 type Handler func(msg *message.Message)
+
+func (peer *Peer) Register(command string, handler Handler) {
+	peer.handlers[command] = handler
+}
